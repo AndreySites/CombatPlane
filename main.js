dac7ce01f781d517845eeaa4fba4be696387c38f
AndreySites <77118972+AndreySites@users.noreply.github.com>
2021-02-25 08:31:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function Download()
 {
-    window.location.href("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing")
+    window.location.replace("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing")
 }
