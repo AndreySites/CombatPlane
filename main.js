5f80f3018b7a336c0312de3f2c76d15dd31721c1
AndreySites <77118972+AndreySites@users.noreply.github.com>
2021-02-25 15:48:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function Download()
 {
-    window.location.replace("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing")
+    var win = window.open("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing", '_blank');
+    win.focus();
 }
