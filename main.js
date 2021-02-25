938f7c80f36a1024c2e2ea403cea00ad8112fa51
AndreySites <77118972+AndreySites@users.noreply.github.com>
2021-02-25 07:57:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-function Download(filename, element)
+function Download()
 {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(" "));
-    element.setAttribute('download', filename);
-  
-    element.style.display = 'none';
-    document.body.appendChild(element);
-  
-    element.click();
-  
-    document.body.removeChild(element);
+    window.location.href("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing")
 }
-  
-// Start file download.
-Download("https://drive.google.com/file/d/1T1drJx4tbi-JgDzYTft3UwTsz1uSJxI6/view?usp=sharing","huh");
