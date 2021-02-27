4dbe4ad9ed76283607a52bcae280bca233d78563
AndreySites <77118972+AndreySites@users.noreply.github.com>
2021-02-27 10:56:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function Download()
+{
+    var win = window.open("https://drive.google.com/file/d/1QcG1TEyeO74n1o0sh2tLsw2Cv4uEhcRp/view?usp=sharing", '_blank');
+    win.focus();
+}
+function Play(){
+    window.location.href = "Game.html"
+}
